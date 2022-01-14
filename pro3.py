6ed4a9bbeb8662d330224ef7ba4b8fedeed5bf08
anmol305 <97669111+anmol305@users.noreply.github.com>
2022-01-14 18:36:04 +0530

    Add files via upload

---
+++ pro3.py
@@ -0,0 +1,14 @@
+print("Stuent data will be stored in the form shown below:")
+print("[SID, Name, Gender, Course Name, CGPA]")
+sid=input("SID:\n")
+name=input("Name:\n").upper()
+gender=input("Gender values: ‘F’(female), ‘M’()male, ‘U’ (For Unknown)\n").upper()
+course=input("Course Name:\n")
+cg=input("CGPA:\n")
+data=[sid,name,gender,course,cg]
+print(data)
+
+
+
+
+
