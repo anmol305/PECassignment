db42175abe2a6b210f616c1d1dc5d2dc9a712729
anmol305 <97669111+anmol305@users.noreply.github.com>
2022-01-14 18:45:46 +0530

    Add files via upload

---
+++ pro4.py
@@ -0,0 +1,8 @@
+a=int(input("enter the marks of student 1:\n"))
+b=int(input("enter the marks of student 2:\n"))
+c=int(input("enter the marks of student 3:\n"))
+d=int(input("enter the marks of student 4:\n"))
+e=int(input("enter the marks of student 5:\n"))
+display=[a,b,c,d,e]
+display.sort()
+print(display)
